4decf02d0f2b459911351c73c082eab2330676c1
Ju de Heer <ju.uemura@gmail.com>
2017-06-22 20:23:06 -0700

    Code to be used as reference for Mission Bit students

---
+++ core/src/com/missionbit/game/sprites/Tube.java
@@ -0,0 +1,65 @@
+package com.missionbit.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.Random;
+
+/**
+ * Created by missionbit on 6/21/17.
+ */
+
+public class Tube {
+    private Texture topTube, bottomTube;
+    private static final int TUBE_GAP = 100; //opening between tubes
+    private static final int LOWEST_OPENING = 120; //lowest position the top of the bottom tube can be, must be above 90 to be above ground level
+    private static final int FLUCTUATION = 130; //may adjust to keep top tube in view
+    public static final int TUBE_WIDTH = 52;
+    private Vector2 posTopTube, posBottomTube;
+    private Random rand;
+    private Rectangle boundsTop, boundsBot;
+
+    public Tube(float x) {
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+        rand = new Random();
+        posTopTube = new Vector2(x, rand.nextInt(FLUCTUATION) + LOWEST_OPENING + TUBE_GAP);
+        posBottomTube = new Vector2(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, topTube.getWidth(), topTube.getHeight());
+        boundsBot = new Rectangle(posBottomTube.x, posBottomTube.y, bottomTube.getWidth(), bottomTube.getHeight());
+    }
+
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBottomTube() {
+        return posBottomTube;
+    }
+
+    public void reposition(float x) {
+        posTopTube.set(x, rand.nextInt(FLUCTUATION) + LOWEST_OPENING + TUBE_GAP);
+        posBottomTube.set(x, posTopTube.y - TUBE_GAP - bottomTube.getHeight());
+        boundsTop.setPosition(posTopTube.x, posTopTube.y);
+        boundsBot.setPosition(posBottomTube.x, posBottomTube.y);
+    }
+
+    public boolean collides(Rectangle player) {
+        return player.overlaps(boundsTop) || player.overlaps(boundsBot);
+    }
+
+    public void dispose() {
+        topTube.dispose();
+        bottomTube.dispose();
+    }
+}
---
+++ core/src/com/missionbit/game/states/MenuState.java
@@ -0,0 +1,50 @@
+package com.missionbit.game.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.missionbit.game.FlappyDemo;
+
+/**
+ * Created by missionbit on 6/20/17.
+ */
+
+public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playBtn.png");
+        cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+    }
+
+    @Override
+    public void handleInput() {
+        if(Gdx.input.justTouched()) {
+            gsm.set(new PlayState(gsm));
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+        sb.draw(background, 0,0);
+        sb.draw(playBtn, cam.position.x - playBtn.getWidth() / 2, cam.position.y);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
+        System.out.println("Disposing of Menu State");
+    }
+}
